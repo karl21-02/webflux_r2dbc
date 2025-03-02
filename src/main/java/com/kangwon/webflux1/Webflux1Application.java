f08742180105acea369d2e5576cffbfed93716be
kimjunhee <manuna530@gmail.com>
2025-03-02 20:50:10 +0900

    **스프링 WebFlux
    WebFlux1로 들어온 요청을 Spring MVC와 WebFlux를 사용해 WebFlux-mvc로 보내고 요청을 반환

--- src/main/java/com/kangwon/webflux1/Webflux1Application.java
+++ src/main/java/com/kangwon/webflux1/Webflux1Application.java
@@ -2,11 +2,26 @@ package com.kangwon.webflux1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @SpringBootApplication
+@RestController
 public class Webflux1Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Webflux1Application.class, args);
+    }
+
+    @GetMapping("/posts/{id}")
+    public Map<String, String> getPosts(@PathVariable Long id) throws Exception {
+        Thread.sleep(3000);
+        if(id > 10L) {
+            throw new Exception("Too Long");
+        }
+        return Map.of("id", id.toString(), "content", "Posts content is %d".formatted(id));
     }
 }
